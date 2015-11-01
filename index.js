1afb978ac272636d8df3c91beb00ddbc84cc4c41
Julien Mineraud <julien.mineraud@gmail.com>
2015-11-01 21:02:13 +0200

    Testing express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello Node.js!\n');
@@ -10,3 +10,17 @@ http.createServer(function (req, res) {
     }
     res.end();
 }).listen(process.env.PORT);
+*/
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
