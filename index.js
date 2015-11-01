4734bab17c405d691f3180d9c22c548d13e56d8e
Julien Mineraud <julien.mineraud@gmail.com>
2015-11-01 21:45:04 +0200

    Checking the restart

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World and more!');
 });
 
 var server = app.listen(process.env.PORT, function () {
