f356944ca38b66d30ad1a246ebd25d1b03ecd856
Julien Mineraud <julien.mineraud@gmail.com>
2015-11-04 23:29:56 +0200

    Front page draft

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+	title: 'Helsinki loves Refugees & Cosmopolitanism', 
+	description: 'Self-organized and self-sufficient collective to help refugees of Helsinki'
+  });
 });
 
 module.exports = router;
